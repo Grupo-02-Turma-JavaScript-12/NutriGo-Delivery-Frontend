3b92e153a39a3d8c2943f9419092aaab83c97924
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2026-03-04 12:18:51 -0300

    navbar e hompage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "./assets/components/navbar/Navbar"
+import Home from "./assets/pages/Home"
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
